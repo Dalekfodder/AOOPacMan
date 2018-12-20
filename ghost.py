553c08f288ae4d21ef04aa1c56ee8ea8dd6d954d
dalekfodder <berk.buzcu@ozu.edu.tr>
2018-12-20 22:42:37 +0300

    Continuation of the refactors

--- ghost.py
+++ ghost.py
@@ -1,16 +1,16 @@
 from character import Character
 from ghost_strategy import *
-
+from pac_man import PacMan
 
 
 class Ghost(Character):
-    def __init__(self, image, x, y, chase_behaviour, scatter_behaviour, pac_man):
+    def __init__(self, image, x, y, chase_behaviour, scatter_behaviour):
         super().__init__(image, x, y)
 
         self.chase_behaviour = chase_behaviour
         self.scatter_behaviour = scatter_behaviour
         self.frightened_behaviour = Frightened
-        self.pac_man = pac_man
+        self.pac_man = PacMan.get_pacman()
 
 
     def perform_chase(self):
@@ -24,20 +24,20 @@ class Ghost(Character):
 
 
 class ConcreteBlinky(Ghost):
-    def __init__(self, x, y, pac_man):
-        super().__init__("images/Blinky.png", x, y, ChaseAggresive, ScatterTopRight, pac_man)
+    def __init__(self, x, y):
+        super().__init__("images/Blinky.png", x, y, ChaseAggresive, ScatterTopRight)
 
 
 class ConcretePinky(Ghost):
-    def __init__(self, x, y, pac_man):
-        super().__init__("images/Pinky.png", x, y, ChaseAmbush, ScatterTopLeft, pac_man)
+    def __init__(self, x, y):
+        super().__init__("images/Pinky.png", x, y, ChaseAmbush, ScatterTopLeft)
 
 
 class ConcreteInky(Ghost):
-    def __init__(self, x, y, pac_man):
-        super().__init__("images/Inky.png", x, y, ChasePatrol, ScatterBottomRight, pac_man)
+    def __init__(self, x, y):
+        super().__init__("images/Inky.png", x, y, ChasePatrol, ScatterBottomRight)
 
 
 class ConcreteClyde(Ghost):
-    def __init__(self, x, y, pac_man):
-        super().__init__("images/Clyde.png", x, y, RandomChase, ScatterBottomLeft, pac_man)
+    def __init__(self, x, y):
+        super().__init__("images/Clyde.png", x, y, RandomChase, ScatterBottomLeft)
