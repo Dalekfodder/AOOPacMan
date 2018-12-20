e463d66fc4602f3fea12ad76ec64d361d1d8e7a3
Dilara <dilaratasyaran@gmail.com>
2018-12-20 14:14:55 +0300

    Concurrent Ghost classes, Blinky, Pinky etc. added.

--- ghost.py
+++ ghost.py
@@ -10,3 +10,31 @@ class Ghost(Character):
         self.chase_behaviour = chase_behaviour
         self.scatter_behaviour = scatter_behaviour
         self.frightened_behaviour = Frightened
+
+    def perform_chase(self):
+        self.chase_behaviour.chase()
+
+    def perform_scatter(self):
+        self.scatter_behaviour.scatter()
+
+    def perform_frightened(self):
+        self.frightened_behaviour.frightened()
+
+class Blinky(Ghost):
+    def __init__(self, image, x, y):
+        super().__init__(image, x, y, ChaseAggresive, ScatterTopRight)
+
+class Pinky(Ghost):
+    def __init__(self, image, x, y):
+        super().__init__(image, x, y, ChaseAmbush, ScatterTopLeft)
+
+class Inky(Ghost):
+    def __init__(self, image, x, y):
+        super().__init__(image, x, y, ChasePatrol, ScatterBottomRight)
+
+class Clyde(Ghost):
+    def __init__(self, image, x, y):
+        super().__init__(image, x, y, RandomChase, ScatterBottomLeft)
+
+
+
